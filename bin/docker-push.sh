576e5e8cc9f7d6744daf09f1b7a815af0774aae1
Johan Palmfjord <johan.palmfjord@hedvig.com>
2020-01-28 16:55:16 +0100

    Fail hard if docker-push fails

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -6,15 +6,15 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
     # This is needed to login on AWS and push the image on ECR
     # Change it accordingly to your docker repo
-    pip install --user awscli
+    pip install --user awscli || exit 1
     export PATH=$PATH:$HOME/.local/bin
-    eval $(aws ecr get-login --no-include-email --region ${AWS_DEFAULT_REGION})
+    eval $(aws ecr get-login --no-include-email --region ${AWS_DEFAULT_REGION}) || exit 1
 
     # Build and push
-    docker build -t $IMAGE_NAME .
+    docker build -t $IMAGE_NAME .  || exit 1
     echo "Pushing $IMAGE_NAME:latest"
-    docker tag $IMAGE_NAME:latest $REMOTE_IMAGE_URL:${TRAVIS_COMMIT}
-    docker push $REMOTE_IMAGE_URL:${TRAVIS_COMMIT}
+    docker tag $IMAGE_NAME:latest $REMOTE_IMAGE_URL:${TRAVIS_COMMIT} || exit 1
+    docker push $REMOTE_IMAGE_URL:${TRAVIS_COMMIT} || exit 1
     echo "Pushed $IMAGE_NAME:${TRAVIS_COMMIT}"
   else
     echo "Skipping deploy because branch is not 'master'"
